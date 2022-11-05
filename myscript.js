eca65de491b63e45cb66c4b458ce06fcae44d7ce
rey301 <abrahamrey3001@gmail.com>
2022-11-05 20:38:31 +0000

    Change variable name in getComputerChoice() function

--- myscript.js
+++ myscript.js
@@ -1,6 +1,6 @@
 function getComputerChoice() {
-    const form = ["rock", "paper", "scissors"];
-    return form[Math.floor(Math.random() * 3)]
+    const computerChoice = ["rock", "paper", "scissors"];
+    return computerChoice[Math.floor(Math.random() * 3)]
 }
 
 function playRound(playerSelection, computerSelection) {
