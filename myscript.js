be7bb143baac8f81e609a8d2db5e5c26e9a5a9a3
rey301 <abrahamrey3001@gmail.com>
2022-11-14 12:07:52 +0000

    Replace result text with scores, add round number

--- myscript.js
+++ myscript.js
@@ -6,44 +6,55 @@ function getComputerChoice() {
 function playRound(playerSelection, computerSelection) {
     let lowerPlayerSelection = playerSelection.toLowerCase();
     if (lowerPlayerSelection === computerSelection) {
-        return "It's a tie!";
+        return 'tie';
     } else if (lowerPlayerSelection === "rock") {
         if (computerSelection === "scissors") {
-            return "You Win! \n You chose: Rock \n CPU chose: Paper";
+            return 'player';
         } else if (computerSelection === "paper") {
-            return "You Lose! Paper beats Rock";
+            return 'comp';
         }
     } else if (lowerPlayerSelection === "paper") {
         if (computerSelection === "rock") {
-            return "You Win! Paper beats Rock";
+            return 'player';
         } else if (computerSelection === "scissors") {
-            return "You Lose! Scissors beats Paper";
+            return 'comp';
         }
     } else if (lowerPlayerSelection === "scissors") {
         if (computerSelection === "paper") {
-            return "You Win! Scissors beats Paper";
+            return 'player';
         } else if (computerSelection === "rock") {
-            return "You Lose! Rock beats Scissors";
+            return 'comp';
         }
     } 
 }
 
-function newImage(imageName, selection) {        
+function newResult(imageName, selection, user, score) {        
     let newImage;
-    
+    let scoreText;
+
     if (document.querySelector(`.${imageName}`) !== null) {
+        scoreText = document.querySelector(`.${user}Score`);
+        scoreText.textContent = score;
+
         newImage = document.querySelector(`.${imageName}`);
         newImage.setAttribute('src', `./images/${selection}.png`);
     } else {
+        scoreText = document.createElement('div');
+        scoreText.classList.add(`${user}Score`);
+        scoreText.textContent = score;
+        document.querySelector(`.${user}`).appendChild(scoreText);
+
         newImage = document.createElement('img');
         newImage.classList.add(imageName);
         newImage.src = `./images/${selection}.png`;
-        document.querySelector('.results').appendChild(newImage);
+        document.querySelector(`.${user}`).appendChild(newImage);
     }
 }
 
 const choices = document.querySelector('.choices').childNodes;
-let chosen = false;
+let playerScore = 0;
+let compScore = 0;
+let round = 0;
 
 choices.forEach(choice => {
     choice.addEventListener('click', e => {
@@ -52,14 +63,23 @@ choices.forEach(choice => {
         as class contains 'hover-over' in the class list 
         */    
         const playerSelection = e.target.getAttribute('class').split(' ')[0];
-        newImage('player-img', playerSelection);
-
-        const computerSelection = getComputerChoice();
-        newImage('comp-img', computerSelection);
+       
+        const computerSelection = getComputerChoice(); 
         
-        const result = document.querySelector('.result-text');
-        result.textContent = playRound(playerSelection, computerSelection);
-        console.log(result.textContent);
+        result = playRound(playerSelection, computerSelection);
+
+        if (result === 'player') {
+            playerScore += 1;
+        } else if (result === 'comp') {
+            compScore += 1;
+        } 
+
+        newResult('player-img', playerSelection, 'player', playerScore);
+        newResult('comp-img', computerSelection, 'comp', compScore);
+
+        round += 1;
+        roundText = document.querySelector('.round');
+        roundText.textContent = `Round: ${round}`;
     });
 
     choice.addEventListener('mouseover', e => {
